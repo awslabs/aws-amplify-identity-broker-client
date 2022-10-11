db12377de81f73954ca5c05b78ccf6a0474e3fe2
Taiki Maekawa <tmae@amazon.com>
2022-10-11 22:38:38 +0900

    updated demo

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+    "hosting": {
+        "S3AndCloudFront": {
+            "Region": "string",
+            "HostingBucketName": "string",
+            "WebsiteURL": "string",
+            "S3BucketSecureURL": "string",
+            "CloudFrontDistributionID": "string",
+            "CloudFrontDomainName": "string",
+            "CloudFrontSecureURL": "string",
+            "CloudFrontOriginAccessIdentity": "string"
+        }
+    }
+}
